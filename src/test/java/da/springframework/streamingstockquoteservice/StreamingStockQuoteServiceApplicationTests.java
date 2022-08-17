377ca6c2133effaca701284a5b85a482aa1015a9
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-17 11:01:53 -0500

    64. Initial Commit.

---
+++ src/test/java/da/springframework/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
